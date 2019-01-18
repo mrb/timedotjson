023eca0bfb86543d56e782802225680d2ea3bb98
Michael Bernstein <michaelrbernstein@gmail.com>
2019-01-18 12:25:02 -0500

    Use helper

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,27 +1,41 @@
-let begin = require('@architect/functions')
-let static = begin.http.helpers.static
+exports.handler = async function http(req) {
+  console.log(req)
 
-async function route(req, res) {
-  try {
-    console.log(req)
+  let img = staticAsset('ornette.jpg')
 
-    let image = static('/ornette.jpg')
+  let body = `
+  <!doctype html>
+  <html>
+    <head>
+      <title>This is fun!</title>
+    </head>
+    <body>Hello ƛ</body>
+    <img src=${img}>
+  </html>
+  `
 
-    let body = `
-    <!doctype html>
-    <html>
-      <head>
-        <title>This is fun!</title>
-      </head>
-      <body>Hello ƛ</body>
-      <img src=${image}>
-    </html>
-    `
-
-    res({ html: body })
-  } catch(e) {
-    console.log(e)
+  return {
+    type: 'text/html; charset=utf8',
+    body
   }
 }
 
-exports.handler = begin.http(route)
+function staticAsset(filename) {
+
+  // these variables are always available to all lambdas
+  let env = process.env.NODE_ENV
+  let app = process.env.ARC_APP_NAME
+
+  // early exit (if we're testing then we can assume the sandbox mounted .static)
+  if (env === 'testing') {
+    return '/' + filename
+  }
+  else {
+    // otherwise use s3 for staging and cloudfront for production
+    let S3Staging = `https://s3-us-west-1.amazonaws.com/begin-functions-staging/${app}`
+    let CFProduction = `https://static.begin.app/${app}`
+    let origin = env === 'staging'? S3Staging : CFProduction
+
+    return `${origin}/${filename}`
+  }
+}
