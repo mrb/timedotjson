0f07b72e844c69ce5551dc63bd9b7909cb7411c2
Michael Bernstein <michaelrbernstein@gmail.com>
2019-01-18 12:10:35 -0500

    Add image

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,69 +1,23 @@
-// Learn how to use secure sessions, middleware, and more:
-// https://docs.begin.com/en/functions/http/
-//
-// let begin = require('@architect/functions')
+let begin = require('@architect/functions')
+let static = begin.http.helpers.static
 
-// TODO: modify the body object!
-let body = `
-<!doctype html>
-<html lang=en>
-  <head>
-    <meta charset=utf-8>
-    <title>Hi!</title>
-    <link rel="stylesheet" href="https://static.begin.app/starter/default.css">
-    <link href="data:image/x-icon;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=" rel="icon" type="image/x-icon">
-  </head>
-  <body>
-
-    <h1 class="center-text">
-      <!-- ↓ Change "Hello world!" to something else and head on back to Begin! -->
-      Hello world!
-    </h1>
-
-    <p class="center-text">
-      Your <a href="https://begin.com" class="link" target="_blank">Begin</a> app is ready to go!
-    </p>
-
-  </body>
-</html>
-`
-
-exports.handler = async function http(req) {
+async function route(req, res) {
   console.log(req)
-  return {
-    type: 'text/html; charset=utf8',
-    body
-  }
+
+  let image = static('/ornette.jpg')
+
+  let body = `
+  <!doctype html>
+  <html>
+    <head>
+      <title>This is fun!</title>
+    </head>
+    <body>Hello ƛ</body>
+    <img src=${image}>
+  </html>
+  `
+
+  res({ html: body })
 }
 
-// Example responses
-
-/* Forward requester to a new path
-exports.handler = async function http(req) {
-  return {
-    status: 302,
-    location: '/about',
-  }
-}
-*/
-
-/* Respond with successful resource creation, CORS enabled
-exports.handler = async function http(req) {
-  return {
-    status: 201,
-    type: 'application/json',
-    body: JSON.stringify({ok: true}),
-    cors: true,
-  }
-}
-*/
-
-/* Deliver client-side JS
-exports.handler = async function http(req) {
-  return {
-    type: 'text/javascript',
-    body: 'console.log("Hello world!")',
-  }
-}
-*/
-
+exports.handler = begin.http(route)
