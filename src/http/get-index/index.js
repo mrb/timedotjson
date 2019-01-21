23696bafc93a8931c201967c7e9a994ac513dba8
Michael Bernstein <michaelrbernstein@gmail.com>
2019-01-21 17:16:19 -0500

    Log like an idiot

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,14 +1,27 @@
+console.log('-2')
+
 let begin = require('@architect/functions')
 
+console.log('-1')
+
 async function auth(req, res, next) {
+  console.log('0')
+
   try {
+    console.log('1')
     if (req.session && req.session.isLoggedIn) {
+      console.log('2')
+
       next()
     }
     else {
+      console.log('3')
+
       res({
         location: '/login'
       })
+      console.log('4')
+
     }
   } catch(e) {
     console.log(e)
@@ -16,11 +29,19 @@ async function auth(req, res, next) {
 }
 
 async function index(req, res) {
+  console.log('5')
+
   try {
+    console.log('6')
+
     res({
       html: `Hi there, you're logged in! <a href="/logout">logout</a>`
     })
+    console.log('7')
+
   } catch(e) {
+    console.log('8')
+
     console.log(e)
   }
 }
