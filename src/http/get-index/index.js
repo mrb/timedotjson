50168fc8222332cfdb38e9c4058e29dc5225b9b2
Michael Bernstein <michaelrbernstein@gmail.com>
2019-01-21 17:12:20 -0500

    Catch errors and hopefully log

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -1,20 +1,28 @@
 let begin = require('@architect/functions')
 
-function auth(req, res, next) {
-  if (req.session && req.session.isLoggedIn) {
-    next()
-  }
-  else {
-    res({
-      location: '/login'
-    })
+async function auth(req, res, next) {
+  try {
+    if (req.session && req.session.isLoggedIn) {
+      next()
+    }
+    else {
+      res({
+        location: '/login'
+      })
+    }
+  } catch(e) {
+    console.log(e)
   }
 }
 
-function index(req, res) {
-  res({
-    html: `Hi there, you're logged in! <a href="/logout">logout</a>`
-  })
+async function index(req, res) {
+  try {
+    res({
+      html: `Hi there, you're logged in! <a href="/logout">logout</a>`
+    })
+  } catch(e) {
+    console.log(e)
+  }
 }
 
 exports.handler = begin.http(auth, index)
